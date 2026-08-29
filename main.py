4fde07c3dbe37b6cc9c9bd27291b9972e3a86c9c
Sanjar Abakirov <sanjar.abakirov@gmail.com>
2026-08-29 23:49:48 +0600

    added module, class, constructo and context manager of the fn

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import zipfile;
 
 # python main.py pack input.txt -o archive.ghzip
 # python main.py unpack archive.ghzip -o output_folder
+
+
+def create_archive(file_name, archive_name):
+    with zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        zipf.write(file_name)
+    print(f"Файл {file_name} успешно архивирован в {archive_name}")
+
+# Пример вызова
+create_archive('document.txt', 'my_archive.zip')
